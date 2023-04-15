f2291eb135388ee02658f804d61696faea5ea8a4
Przemek Koscinski <przem.koscinski@gmail.com>
2023-04-15 12:23:08 +0200

    DTO class added

--- src/main/java/com/example/springfinalproject/dto/PlaceDto.java
+++ src/main/java/com/example/springfinalproject/dto/PlaceDto.java
@@ -1,4 +1,17 @@
 package com.example.springfinalproject.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class PlaceDto {
+
+        private Long id;
+        private String name;
+        private String floor;
+        private boolean availability;
+        private int sittingPlace;
+        private int standingPlace;
+        private int lyingPlace;
 }
