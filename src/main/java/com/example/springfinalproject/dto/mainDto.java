73e128a8648bc1470d237d5df71098563dbbe592
Oskar <o.piechowski@gmail.com>
2023-04-15 10:23:11 +0200

    firstfilles - architecture.

---
+++ src/main/java/com/example/springfinalproject/dto/mainDto.java
@@ -0,0 +1,10 @@
+package com.example.springfinalproject.dto;
+
+public class mainDto {
+    private long id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+}
