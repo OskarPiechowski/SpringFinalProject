d631708d843b0d79a829bb80ef125d9cc5ec2a9d
Maciej Lipinski <maciej.lipinski123@gmail.com>
2023-05-19 11:21:26 +0200

    add @Valid in controller

--- src/main/java/com/example/springfinalproject/controller/AdditionalEquipmentController.java
+++ src/main/java/com/example/springfinalproject/controller/AdditionalEquipmentController.java
@@ -3,6 +3,7 @@ package com.example.springfinalproject.controller;
 import com.example.springfinalproject.dto.AdditionalEquipmentDto;
 import com.example.springfinalproject.entity.RoomReservation;
 import com.example.springfinalproject.service.AdditionalEquipmentService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,7 +23,7 @@ public class AdditionalEquipmentController {
     }
 
     @PostMapping("/add-equipment")
-    public String createAdditionalEquipment(AdditionalEquipmentDto additionalEquipmentDto, Model model) {
+    public String createAdditionalEquipment(@Valid AdditionalEquipmentDto additionalEquipmentDto, Model model) {
         System.out.println(additionalEquipmentDto);
         additionalEquipmentService.addAdditionalEquipment(additionalEquipmentDto);
         model.addAttribute("message", "Dosałeś wyposażenie dodatkowe");
