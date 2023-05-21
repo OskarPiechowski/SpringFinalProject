c229d9d8857b907e87f6928d55ae793db7b39cd4
Oskar <o.piechowski@gmail.com>
2023-05-21 13:36:30 +0200

    * invoice list but again invoiceServicenullpointer :<

--- src/main/java/com/example/springfinalproject/repository/InvoiceRepository.java
+++ src/main/java/com/example/springfinalproject/repository/InvoiceRepository.java
@@ -1,7 +1,11 @@
 package com.example.springfinalproject.repository;
 
 import com.example.springfinalproject.entity.Invoice;
+import com.example.springfinalproject.entity.RoomReservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface InvoiceRepository extends JpaRepository<Invoice,Long> {
+
 }
--- src/main/java/com/example/springfinalproject/service/InvoiceService.java
+++ src/main/java/com/example/springfinalproject/service/InvoiceService.java
@@ -7,14 +7,34 @@ import com.example.springfinalproject.repository.InvoiceRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
+@Transactional
 @AllArgsConstructor
 public class InvoiceService {
-private InvoiceRepository invoiceRepository;
-private InvoiceMapper invoiceMapper;
+        private InvoiceRepository invoiceRepository;
+        private InvoiceMapper invoiceMapper;
+
         public void addInvoice(InvoiceDto request) {
                 Invoice invoice = invoiceMapper.mapToEntity(request);
                 invoiceRepository.save(invoice);
 
-}
+        }
+
+        public List<Invoice> findAllInvoices() {
+                return invoiceRepository.findAll();
+
+/*        public List<InvoiceDto> findAllInvoices() {
+                List<Invoice> invoices = invoiceRepository.findAll();
+                List<InvoiceDto> invoiceDtos = new ArrayList<>();
+                for(Invoice invoice : invoices){
+                        invoiceDtos.add(invoiceMapper.mapToDto(invoice));
+                }
+                return invoiceDtos;
+
+        }*/
+        }
 }
