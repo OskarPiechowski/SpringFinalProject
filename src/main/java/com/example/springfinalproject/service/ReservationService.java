c0712bd2f23621b7a6412163c780d6d3f0d9ca2f
Maciej Lipinski <maciej.lipinski123@gmail.com>
2023-06-03 20:39:05 +0200

    refactor reservation method to use Dto argument

--- src/main/java/com/example/springfinalproject/service/ReservationService.java
+++ src/main/java/com/example/springfinalproject/service/ReservationService.java
@@ -49,7 +49,7 @@ public class ReservationService {
     public void createReservation(RoomReservation reservation) {
     }
 
-    public void setReservation(long roomId, RoomReservation reservation) {
+    public void setReservation(long roomId, RoomReservationDto roomReservationDto) {
         Optional<Organisation> loggedOrganisation = authenticationService.selectLoggedOrganisation();
         Organisation organisation = loggedOrganisation.get();
         RoomReservation roomReservation = roomReservationMapper.mapToEntity(roomReservationDto);
